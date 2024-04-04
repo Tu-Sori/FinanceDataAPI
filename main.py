74ddb210a7ee7e19e9c3b3e36980af5ecb1d4c1d
zzoo0123 <0123suh@gmail.com>
2024-04-04 19:55:11 +0900

    feat: cors 처리
    3000port

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from domain import home, sic, mypage
 from database.database import engine
 from database import models
@@ -7,6 +8,17 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
 
 app.include_router(home.router)
 app.include_router(sic.router)
@@ -15,3 +27,4 @@ app.include_router(mypage.router)
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
